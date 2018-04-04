06ee555577a14d05779fb9c765b707c550ea5127
Jace Mitsuka <PJace@BigMac.local>
2018-04-04 10:00:21 -1000

    completed fib assignment

--- even_fib_nums.js
+++ even_fib_nums.js
@@ -6,13 +6,29 @@
  * @return {Number} sum
  */
 function _sumFibs( maxFibValue ) {
-  var sum = 0;
-
-  // do your work here
-
-  return sum;
+  let sum = 0;
+  let fibArr = [];
+  let filteredArr = [];
+  let a = 0;
+  let b = 1;
+  let f = 1;
+  for (let i = 2; i <= maxFibValue; i++) {
+    f = a + b;
+    a = b;
+    b = f;
+    fibArr.push(f)
+  }
+  for (let z = 0; z <= maxFibValue; z++) {
+    if (fibArr[z] <= maxFibValue) {
+      filteredArr.push(fibArr[z]);
+    }
+  }
+  for (let t = 0; t < filteredArr.length; t++) {
+    if (filteredArr[t] % 2 === 0) {
+      sum += filteredArr[t]
+    }
+  } return sum
 }
-
 // bonus round
 function _highestFibonacciNumber (maxFibValue){
   var highest = 0;
@@ -33,3 +49,18 @@ module.exports = {
   sumFibs : _sumFibs,
   highestFibonacciNumber : _highestFibonacciNumber
 };
+
+
+function fib(maxFibValue){
+  var a = 0;
+  var b = 1;
+  var f = 1;
+   while (i <= maxFibValue) {
+    f = a + b;
+    a = b;
+    b = f;
+  }
+
+
+
+}
